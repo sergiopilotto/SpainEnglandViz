14775da5bfa059787ba29e2270a53dba33fb5f32
Sergio Pilotto Manresa <sergiopm2003@gmail.com>
2024-10-09 21:54:04 +0200

    Add second analysis xT

--- SpainEnglandVizPython/xTAnalysis.py
+++ SpainEnglandVizPython/xTAnalysis.py
@@ -26,14 +26,33 @@ df_playersxT = pd.DataFrame()
 df_playersxT.insert(0, "players", df.loc[:, 'shortName'].unique())
 df_playersxT.dropna(inplace=True)
 
+players_spain = df.groupby('teamName').get_group('Spain').loc[:,'shortName'].dropna().unique()
+players_england = df.groupby('teamName').get_group('England').loc[:,'shortName'].dropna().unique()
+
+
 df_playersxT.insert(1, "xT_mean", 0)
 df_playersxT.insert(2, "xT_total", 0)
+df_playersxT.insert(3, 'teamName', '')
+
+
 
 for i, row in df_playersxT.iterrows():
     mean = df.groupby('shortName').get_group(row['players'])['xT'].mean()
     df_playersxT.at[i, 'xT_mean'] = mean
     total = df.groupby('shortName').get_group(row['players'])['xT'].sum()
     df_playersxT.at[i, 'xT_total'] = total
+    if (row['players'] in players_spain):
+        df_playersxT.at[i, 'teamName'] = 'Spain'
+    else:
+        df_playersxT.at[i, 'teamName'] = 'England'
 
 df_playersxT.dropna(inplace=True)
-df_playersxT = df_playersxT.sort_values(by='xT_total', ascending=False).reset_index()
+df_playersxT = df_playersxT.sort_values(by='xT_mean', ascending=False).reset_index()
+
+
+
+fig = plt.figure(figsize=[34, 20])
+
+sns.barplot(data=df_playersxT, x='players', y='xT_mean', hue='teamName', palette=["#9c1f1f", "#1eadba"])
+plt.show()
+
---
+++ SpainEnglandVizPython/xTEvolution.py
@@ -0,0 +1,99 @@
+import mplsoccer
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib.pyplot import legend
+
+sc = "#9c1f1f" # spain color
+ec = "#1eadba" # england color
+
+data = pd.read_csv("EurosFinal2024-Spain v England.csv")
+df = data.loc[:, ['expandedMinute', 'second', 'type',
+                'outcomeType', 'shortName', 'xT',
+                'x', 'y', 'endX', 'endY', 'teamName']]
+
+df.dropna(inplace=True)
+
+xT_spain = 0
+xT_england = 0
+stopMinute = 1
+var_xTS = []
+var_xTE = []
+evo_min = []
+
+for i, row in df.iterrows():
+    if row['teamName'] == 'Spain':
+        xT_spain += row['xT']
+    else :
+        xT_england += row['xT']
+    if row['expandedMinute'] > stopMinute:
+        var_xTS.append(xT_spain)
+        var_xTE.append(xT_england)
+        xT_spain = 0
+        xT_england = 0
+        stopMinute = row['expandedMinute'] + 1
+        evo_min.append(row['expandedMinute'])
+
+
+fig = plt.figure(figsize = (15, 10))
+
+plt.plot(evo_min, var_xTS, linestyle='-', c='r', label='Spain')
+plt.plot(evo_min, var_xTE, linestyle='-', c='c', label='England')
+
+ax = fig.gca()
+ax.set_xticks(np.arange(0, 96, 3), minor=True)
+ax.set_xticks(np.arange(0, 96, 3))
+ax.set_yticks(np.arange(-0.2,0.4, 0.05))
+ax.set_yticks(np.arange(-0.2,0.4, 0.05), minor=True)
+plt.grid()
+
+plt.xlabel('Minutes')
+plt.ylabel('xT')
+
+plt.title('Expected threat (xT) cumulated by minute', fontsize = 20, fontweight = 'bold')
+plt.legend(loc='best')
+
+plt.xlim(0, 96)
+plt.ylim(-0.2, 0.4)
+#plt.show()
+
+
+df.sort_values('xT', ascending=False, inplace=True)
+df_top10xT = df.head(10)
+pitch = mplsoccer.Pitch(pitch_type='uefa', pitch_color='black', line_color='white')
+fig, ax = pitch.draw(figsize = (15, 10))
+fig.set_facecolor('black')
+
+pitch.arrows(df_top10xT.groupby('teamName').get_group('Spain').x,
+             df_top10xT.groupby('teamName').get_group('Spain').y,
+             df_top10xT.groupby('teamName').get_group('Spain').endX,
+             df_top10xT.groupby('teamName').get_group('Spain').endY,
+             ax=ax, color=sc, label = 'Spain')
+pitch.arrows(105-df_top10xT.groupby('teamName').get_group('England').x,
+             df_top10xT.groupby('teamName').get_group('England').y,
+             105-df_top10xT.groupby('teamName').get_group('England').endX,
+             df_top10xT.groupby('teamName').get_group('England').endY,
+             ax=ax, color=ec, label = 'England')
+
+
+
+for i, row in df_top10xT.iterrows():
+    if row['teamName'] == 'Spain':
+        ax.annotate(row['shortName'] + ' ('+str(round(row['xT'], 3)) + ')',
+                    (row['x'], row['y']+1), color='grey', weight='bold')
+    else:
+        ax.annotate(row['shortName'] + ' ('+str(round(row['xT'], 3)) + ')',
+                    (105-row['x'], row['y']+1), color='grey', weight='bold')
+
+
+
+
+legend = plt.legend(prop={'size': 15}, labelcolor='white', edgecolor='black', bbox_to_anchor=(0.9, 1.05), loc='upper left')
+legend.get_frame().set_alpha(None)
+legend.get_frame().set_facecolor((0, 0, 0, 0))
+plt.title('The 10 passes with most xT', fontsize = 30, fontweight = 'bold', color = 'white')
+plt.show()
+
+
+
